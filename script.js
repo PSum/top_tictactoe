5efa6e602a9d14c044faa47df1c23a31c320143c
PSum <sum.philipp@gmail.com>
2023-10-14 16:07:06 +0200

    update

--- script.js
+++ script.js
@@ -10,12 +10,26 @@ function createPlayer (name){
 
 function gameboard (){
     return{
-        board: [[0,0,0],[0,0,0],[0,0,0]],
+        board: [0,0,0,0,0,0,0,0,0],
         showBoard: function(){
             console.log(this.board);
     },
 };
 }
 
+function scanField (){
+    const fields = document.querySelectorAll('.field');
+    fields.forEach((field) => {
+        field.addEventListener('click', (event) => {
+            const clickedButton = event.target;
+            const buttonId = clickedButton.id;
+            console.log(buttonId);
+        });
+    });
+}
+
+scanField();
+
 const board = gameboard();
 console.log(board.board);
+
